98b7fcd673eb4ab34a5989d2a3d8e80940313f23
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2019-11-01 15:53:48 +0100

    Adding:- Countdown timer - Change backgroundcolor - Start/pause button

---
+++ src/components/Timer.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import './Timer.css';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(40);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  function backgroundColor () {
+    if (40 >= seconds) {
+      document.getElementById("countdown-app").style.background ="blue";
+      if (20 >= seconds) {
+        document.getElementById("countdown-app").style.background ="orange";
+        if(12 >= seconds) {
+          document.getElementById("countdown-app").style.background ="red";
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+      if (seconds === 0){
+        reset();
+      }
+      backgroundColor();
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  return (
+    <div className="time-app" id="countdown-app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
